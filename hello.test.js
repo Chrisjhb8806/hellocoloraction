dafe3c892099f830c256ba98cd9e6aa6494c77a9
Chrisjhb8806 <chris.vincent@hotmail.com>
2024-01-26 15:27:54 +0200

    Hello World from South Africa!

--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,5 @@
 const hello = require("./hello");
-describe("My hello", () => {
+describe("Hello World from South Africa!", () => {
   test("works", () => {
     expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
   });
